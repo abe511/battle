bf27468d3227bfeb94691ad2c2d8eafdbf4a96a9
Art <absolutus11@gmail.com>
2021-11-30 02:18:36 +0400

    attack speed fixed

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const faker = require('faker/locale/it');
 
 class Fighter {
   constructor() {
-    // this.name = faker.name.findName();
     this.name = faker.name.firstName();
+    // this.name = faker.name.findName(); // uncomment fo full name
     this.health = randRange(80, 100);
     this.initialHealth = this.health;
     this.power = randRange(2, 5, 1);
@@ -25,11 +25,14 @@ const randInt = (max=10) => {
 }
 
 
+// decrease speed according to health level
 const calculateSpeed = (fighter) => {
   const speed = fighter.initialSpeed * (fighter.health / fighter.initialHealth);
   return parseFloat(speed.toFixed(3));
 };
 
+
+// generate fighters, start and restart the battle 
 const start = () => {
   const totalFighters = 10;
   const fighters = [];
@@ -43,6 +46,7 @@ const start = () => {
   restart(fighters, allAttacks);
 }
 
+// pick random opponent for every fighter and attack
 const restart = (fighters, allAttacks) => {
   fighters.forEach((f1) => {
       let f2 = f1;
@@ -52,11 +56,14 @@ const restart = (fighters, allAttacks) => {
   });
 };
 
+// stop all timers
 const stop = (allAttacks) => {
   for(let i = 0; i < allAttacks.length; ++i) {
     clearTimeout(allAttacks[i]);
   }
 }
+
+
 
 const attack = (f, f1, f2, allAttacks) => {
 
@@ -64,24 +71,24 @@ const attack = (f, f1, f2, allAttacks) => {
   f1.speed = calculateSpeed(f1);
   f2.speed = calculateSpeed(f2);
 
+  // furious mode ON
   if(f2.health > 0 && f2.health <= 30) {
-    f2.speed = parseFloat((f2.speed * 3).toFixed(3));
-  } else if (f2.health > 30) {
-    f2.speed = f2.initialSpeed;
+    f2.speed = parseFloat(Math.min(f2.speed * 3, 5).toFixed(3));
   }
 
   if(f2.health <= 0) {
     // all timers cleared
     stop(allAttacks);
     console.log(`[${f2.name}] is dying`);
-    // dramatic moment
+    // drama starts here
     const decision = parseInt(Math.random().toFixed()) ? '“Live”' : '“Finish him”';
     console.log(`Caesar showed ${decision} to [${f2.name}]`);
+    // increase opponents health and continue
     if(decision === '“Live”') {
       f2.health += 50;
       restart(f, allAttacks);
     } else {
-      // remove killed fighter;
+      // remove killed opponent;
       let newFighters = f.filter((fighter) => {
         return fighter.name !== f2.name;
       });
@@ -93,15 +100,16 @@ const attack = (f, f1, f2, allAttacks) => {
       restart(newFighters, allAttacks);
     }
   } else {
-    // choose random fighter
     if(f1.health > 0) {
+      // report action and decrease opponents health
       console.log(`[${f1.name} x ${f1.health}] hits [${f2.name} x ${f2.health}] with power ${f1.power}`);
       f2.health = parseInt((f2.health - f1.power).toFixed());
+      // choose random opponent
       f2 = f1;
       while(f1 === f2)
         f2 = f[randInt(f.length)];
+      // calculate timing, collect timeouts and attack
       delay = parseInt((5000 / f1.speed).toFixed());     
-      // console.log(delay);
       const timeout = setTimeout(attack, delay, f, f1, f2, allAttacks);
       allAttacks.push(timeout);
     }
